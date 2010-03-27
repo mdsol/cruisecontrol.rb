da9c85b8482e9fa9ac2c21fb547d5bed29c68dde
petitbon <whatever8@gmail.com>
2010-03-27 12:06:14 -0400

    added capistrano and chnaged the look

---
+++ config/deploy.rb
@@ -0,0 +1,177 @@
+# Capistrano deploy specification.
+# Integrated with poolparty by getting node addresses from poolparty, and use them to set cap roles.
+
+# CAPISTRANO APPLICATION DEPLOYMENT 
+
+# run: cap deploy:setup
+# run: cap deploy
+
+
+# -------------------------------------------------------------------
+# SET DEPLOYMENT ENVIRONMENT - ASK THE DEVELOPER
+# -------------------------------------------------------------------
+
+# Whatever you set here will be taken and set as the default RAILS_ENV value
+
+
+# colorize capistrano output
+require 'capistrano_colors'
+
+capistrano_color_matchers = [
+  { :match => /command finished/,       :color => :hide,      :prio => 10 },
+  { :match => /executing command/,      :color => :blue,      :prio => 10, :attribute => :underscore },
+  { :match => /^transaction: commit$/,  :color => :magenta,   :prio => 10, :attribute => :blink },
+  { :match => /svn/,                    :color => :white,     :prio => 20, :attribute => :reverse },
+]
+
+colorize( capistrano_color_matchers )
+
+
+set :rails_env, "production" 
+
+# -------------------------------------------------------------------
+# GENERAL SETTINGS
+# -------------------------------------------------------------------
+
+set :application, "medistrano.vpc.com"
+
+set :use_sudo, true
+
+set :user, "ubuntu"
+
+set :port, 22
+
+default_run_options[:pty] = true
+
+ssh_options[:paranoid] = false 
+ssh_options[:keys] =  "~/.ec2/MACHINE-cruise"
+ssh_options[:forward_agent] = true
+#ssh_options[:user] =  "ubuntu"
+
+# -------------------------------------------------------------------
+# SUBVERSIONING
+# -------------------------------------------------------------------
+
+set :scm, "git"
+set :repository, "git@github.com:mdsolgithubadmin/cruisecontrol.rb.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+#set :deploy_via, :remote_cache
+
+# -------------------------------------------------------------------
+# ROLES
+# -------------------------------------------------------------------
+
+role :web,      "10.225.50.7"
+role :app,      "10.225.50.7"
+role :db,       "10.225.50.7", :primary => true
+
+
+# -------------------------------------------------------------------
+# DEPLOY
+# -------------------------------------------------------------------
+
+deploy_dir = "/mnt/cruisecontrol"
+
+set :deploy_to, deploy_dir
+
+set :keep_releases, 10
+
+
+# -------------------------------------------------------------------
+# Passenger
+# -------------------------------------------------------------------
+
+
+deploy.task :restart, :roles => :app do
+  run "#{latest_release}/cruise stop"
+  run "#{latest_release}/cruise start -d"
+  #run "sudo chown -R ubuntu:ubuntu #{latest_release}/tmp"
+  nginx.restart
+  
+  #run "touch #{current_path}/tmp/restart.txt"
+end
+
+
+
+
+
+
+
+# -------------------------------------------------------------------
+# nginx
+# -------------------------------------------------------------------
+
+after "deploy:setup", "nginx:setup"
+
+namespace :nginx do
+
+  task :start do
+    run "sudo /etc/init.d/nginx start"
+  end
+
+  task :stop do
+    run "sudo /etc/init.d/nginx stop"
+  end
+
+  task :restart do
+    run "sudo /etc/init.d/nginx restart"
+  end
+  
+  task :setup do
+    nginx.stop
+    run "sudo chown -R ubuntu:ubuntu /mnt/cruisecontrol"
+    run "sudo rm -f /opt/nginx/conf/sites-enabled/cruisecontrol.conf"
+    run "sudo rm -f /opt/nginx/conf/sites-available/cruisecontrol.conf"
+    template_path     = File.expand_path(File.join(__FILE__,"..", "nginx.conf.erb"))
+    compiled_template = ERB.new(File.read(template_path)).result(binding)
+    sudo_put(compiled_template, "/opt/nginx/conf/sites-available/cruisecontrol.conf")
+    run "sudo ln -s /opt/nginx/conf/sites-available/cruisecontrol.conf /opt/nginx/conf/sites-enabled/cruisecontrol.conf"  
+    nginx.start
+    nginx.restart
+  end
+  
+end
+
+# -------------------------------------------------------------------
+# SETTTINGS
+# -------------------------------------------------------------------
+
+#after "deploy:update_code", "deploy:symlink_config"
+#after "deploy:update_code", "deploy:symlink_db" 
+#
+#namespace :deploy do
+#  desc "Symlink to the right database yaml"
+#  task :symlink_db, :roles => :app do
+#    run "ln -nfs #{release_path}/config/database.yml.production #{release_path}/config/database.yml"
+#  end
+#  desc "Symlink to the production webistrano configuration file" 
+#  task :symlink_config, :roles => :app do
+#    run "sudo mv #{release_path}/config/webistrano_config_production.rb #{release_path}/config/webistrano_config.rb" 
+#  end
+#end
+
+
+# -------------------------------------------------------------------
+# HELPERS
+# -------------------------------------------------------------------
+
+def sudo_put(data, target)
+  tmp = "#{shared_path}/~tmp-#{rand(9999999)}"
+  put data, tmp
+  on_rollback { run "rm #{tmp}" }
+  sudo "cp -f #{tmp} #{target} && rm #{tmp}"
+end
+
+
+# config/deploy.rb
+after "deploy:update_code", "gems:install"
+ 
+namespace :gems do
+  desc "Install gems"
+  task :install, :roles => :app do
+    run "cd #{current_release} && #{sudo} rake gems:install RAILS_ENV=#{rails_env} "
+  end
+end
+
+
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,3 +12,82 @@ function disableBuildNowButton(button) {
   button.className='build_button_disabled';
   button.disabled = true;
 }
+
+
+
+
+
+
+
+var cX = 0; var cY = 0; var rX = 0; var rY = 0;
+
+function UpdateCursorPosition(e){ 
+	cX = e.pageX; 
+	cY = e.pageY;
+}
+
+function UpdateCursorPositionDocAll(e){ 
+	cX = event.clientX; 
+	cY = event.clientY;
+}
+
+if(document.all) { 
+	document.onmousemove = UpdateCursorPositionDocAll; 
+} else { 
+	document.onmousemove = UpdateCursorPosition; 
+}
+
+function AssignPosition(d, pX, pY) {
+	
+	if(self.pageYOffset) {
+		rX = self.pageXOffset;
+		rY = self.pageYOffset;
+  }
+	else if(document.documentElement && document.documentElement.scrollTop) {
+		rX = document.documentElement.scrollLeft;
+		rY = document.documentElement.scrollTop;
+  }
+	else if(document.body) {
+		rX = document.body.scrollLeft;
+		rY = document.body.scrollTop;
+  }
+	if(document.all) {
+		cX += rX; 
+		cY += rY;
+	}
+	
+	if(cY < 100){
+		/*top display where raider name link is on top menu
+     /* removed cY cord in d.style.top reinstate like this cY+pY */
+		//pX = 300;
+		//pY = 20;
+		//d.style.left = (cX-pX) + "px";
+		//d.style.top = (30) + "px";
+		
+    d.style.left = (420) + "px";
+    d.style.top = (0) + "px";
+		
+	} else {
+		/*Bottom display where room of the day is
+     /* removed cY cord in d.style.top reinstate like this cY+pY */
+		pX = 100;
+		pY = -120;
+		d.style.left = (cX-pX) + "px";
+		d.style.top = (305) + "px";
+	}
+  
+}
+
+
+function HideTipTop(phantomline) {
+	if(phantomline.length < 1) { return; }
+	document.getElementById(phantomline).style.display = "none";
+	document.getElementById(phantomline).style.top = "";
+}
+
+function ShowTipTop(phantomline) {
+	if(phantomline.length < 1) { return; }
+	var phantomline = document.getElementById(phantomline);  
+	AssignPosition(phantomline);
+	phantomline.style.display = "block";
+}
